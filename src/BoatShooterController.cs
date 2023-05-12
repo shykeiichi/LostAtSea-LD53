d0299540327e85c61f9ee436805f12c16cf84d60
Willem Dinkelspiel <22widi@ad.ssis.nu>
2023-05-12 13:05:48 +0200

    asd

--- src/BoatShooterController.cs
+++ src/BoatShooterController.cs
@@ -11,10 +11,10 @@ class BoatShooterController : Component
 
     public void Shoot()
     {
-        ParentScene.RegisterEntity(new Entity(ParentScene).Add(new CannonBallController(transform.Position, transform.Angle + 85, 100f)));
-        ParentScene.RegisterEntity(new Entity(ParentScene).Add(new CannonBallController(transform.Position, transform.Angle + 95, 100f)));
+        ParentScene.RegisterEntity(new Entity(ParentScene).ExcludeFromInspector(true).Add(new CannonBallController(transform.Position, transform.Angle + 85, 100f)));
+        ParentScene.RegisterEntity(new Entity(ParentScene).ExcludeFromInspector(true).Add(new CannonBallController(transform.Position, transform.Angle + 95, 100f)));
 
-        ParentScene.RegisterEntity(new Entity(ParentScene).Add(new CannonBallController(transform.Position, transform.Angle + 265, 100f)));
-        ParentScene.RegisterEntity(new Entity(ParentScene).Add(new CannonBallController(transform.Position, transform.Angle + 275, 100f)));
+        ParentScene.RegisterEntity(new Entity(ParentScene).ExcludeFromInspector(true).Add(new CannonBallController(transform.Position, transform.Angle + 265, 100f)));
+        ParentScene.RegisterEntity(new Entity(ParentScene).ExcludeFromInspector(true).Add(new CannonBallController(transform.Position, transform.Angle + 275, 100f)));
     }   
 }
